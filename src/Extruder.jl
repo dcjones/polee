d8e0ce8e1e532483128725f3f1dbb6c328a3bcd0
Daniel C. Jones <dcjones@cs.washington.edu>
2017-08-31 16:20:33 -0700

    Move some functions to separate file.

--- src/Extruder.jl
+++ src/Extruder.jl
@@ -34,6 +34,7 @@ include("likelihood-approximation.jl")
 include("estimate.jl")
 include("gibbs.jl")
 include("stick_breaking.jl")
+include("sequences.jl")
 
 # TODO: automate including everything under models
 EXTRUDER_MODELS = Dict{String, Function}()
@@ -44,68 +45,6 @@ include("models/logistic-regression.jl")
 include("models/simple-logistic-regression.jl")
 include("models/quantification.jl")
 include("models/pca.jl")
-
-
-function read_transcript_sequences!(ts, filename)
-    if endswith(filename, ".2bit")
-        read_transcript_sequences_from_twobit!(ts, filename)
-    else
-        read_transcript_sequences_from_fasta!(ts, filename)
-    end
-end
-
-
-function read_transcript_sequences_from_fasta!(ts, filename)
-    prog = Progress(filesize(filename), 0.25, "Reading sequences ", 60)
-    reader = open(FASTA.Reader, filename)
-    entry = eltype(reader)()
-
-    i = 0
-    while !isnull(tryread!(reader, entry))
-        seqname = FASTA.identifier(entry)
-        if length(entry.sequence) > 100000
-            ProgressMeter.update!(prog, position(reader.state.stream.source))
-        end
-
-        if haskey(ts.trees, seqname)
-            entryseq = FASTA.sequence(entry)
-            for t in ts.trees[seqname]
-                seq = t.metadata.seq
-                for exon in t.metadata.exons
-                    if exon.last <= length(entryseq)
-                        append!(seq, entryseq[exon.first:exon.last])
-                    end
-                end
-            end
-        end
-    end
-    finish!(prog)
-end
-
-
-function read_transcript_sequences_from_twobit!(ts, filename)
-    reader = open(TwoBit.Reader, filename)
-    prog = Progress(length(ts.trees), 0.25, "Reading sequences ", 60)
-
-    for (i, (name, tree)) in enumerate(ts.trees)
-        ProgressMeter.update!(prog, i)
-        local refseq
-        try
-            refseq = reader[name].seq
-        catch
-            continue
-        end
-
-        for t in tree
-            seq = t.metadata.seq
-            for exon in t.metadata.exons
-                if exon.last <= length(refseq)
-                    append!(seq, DNASequence(refseq[exon.first:exon.last]))
-                end
-            end
-        end
-    end
-end
 
 
 function print_usage()
---
+++ src/sequences.jl
@@ -0,0 +1,62 @@
+
+function read_transcript_sequences!(ts, filename)
+    if endswith(filename, ".2bit")
+        read_transcript_sequences_from_twobit!(ts, filename)
+    else
+        read_transcript_sequences_from_fasta!(ts, filename)
+    end
+end
+
+
+function read_transcript_sequences_from_fasta!(ts, filename)
+    prog = Progress(filesize(filename), 0.25, "Reading sequences ", 60)
+    reader = open(FASTA.Reader, filename)
+    entry = eltype(reader)()
+
+    i = 0
+    while !isnull(tryread!(reader, entry))
+        seqname = FASTA.identifier(entry)
+        if length(entry.sequence) > 100000
+            ProgressMeter.update!(prog, position(reader.state.stream.source))
+        end
+
+        if haskey(ts.trees, seqname)
+            entryseq = FASTA.sequence(entry)
+            for t in ts.trees[seqname]
+                seq = t.metadata.seq
+                for exon in t.metadata.exons
+                    if exon.last <= length(entryseq)
+                        append!(seq, entryseq[exon.first:exon.last])
+                    end
+                end
+            end
+        end
+    end
+    finish!(prog)
+end
+
+
+function read_transcript_sequences_from_twobit!(ts, filename)
+    reader = open(TwoBit.Reader, filename)
+    prog = Progress(length(ts.trees), 0.25, "Reading sequences ", 60)
+
+    for (i, (name, tree)) in enumerate(ts.trees)
+        ProgressMeter.update!(prog, i)
+        local refseq
+        try
+            refseq = reader[name].seq
+        catch
+            continue
+        end
+
+        for t in tree
+            seq = t.metadata.seq
+            for exon in t.metadata.exons
+                if exon.last <= length(refseq)
+                    append!(seq, DNASequence(refseq[exon.first:exon.last]))
+                end
+            end
+        end
+    end
+end
+

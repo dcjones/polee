b27cebcac162f90b3982b7a08a57c357e566c123
Daniel C. Jones <dcjones@cs.washington.edu>
2018-03-10 10:15:43 -0800

    Batch effect correction for gplvm

--- src/models/gplvm.jl
+++ src/models/gplvm.jl
@@ -1,17 +1,58 @@
 
-function estimate_gplvm(input::ModelInput, num_components::Int=2)
+function estimate_gplvm(input::ModelInput; num_components::Int=2,
+                        correct_batch_effects::Bool=false)
     if input.feature != :transcript
         error("GPLVM only implemented with transcripts")
     end
 
-    sess = ed.get_session()
-
     num_samples, n = size(input.loaded_samples.x0_values)
 
+    # build design matrix
+    # -------------------
+
+    factoridx = Dict{String, Int}()
+    for factors in input.loaded_samples.sample_factors
+        for factor in factors
+            get!(factoridx, factor, length(factoridx) + 1)
+        end
+    end
+
+    num_factors = length(factoridx)
+    X_ = zeros(Float32, (num_samples, num_factors))
+    for i in 1:num_samples
+        for factor in input.loaded_samples.sample_factors[i]
+            j = factoridx[factor]
+            X_[i, j] = 1
+        end
+    end
+    X = tf.constant(X_)
+
+    # batch effects
+    # -------------
+
+    x_mu_batch = nothing
+
+    if correct_batch_effects
+        w_batch_mu0 = 0.0f0
+        w_batch_sigma0 = 2.0f0
+
+        w_batch_sigma = tf.constant(w_batch_sigma0, shape=[num_factors, n])
+        w_batch_mu = tf.constant(w_batch_mu0, shape=[num_factors, n])
+
+        w_batch = edmodels.Normal(name="w_batch", loc=w_batch_mu, scale=w_batch_sigma)
+        x_mu_batch = tf.matmul(X, w_batch)
+    end
+
+    # bias
+    # ----
+
     x_mu_bias_mu0 = log(1f0/n)
-    x_mu_bias_sigma0 = 2.5f0
+    x_mu_bias_sigma0 = 4.0f0
     x_mu_bias = edmodels.Normal(loc=tf.fill([1, n], x_mu_bias_mu0),
-                              scale=tf.fill([1, n], x_mu_bias_sigma0))
+                                scale=tf.fill([1, n], x_mu_bias_sigma0))
+
+    # gplvm
+    # -----
 
     z = edmodels.Normal(loc=tf.zeros([num_samples, num_components]),
                         scale=tf.fill([num_samples, num_components], 1.0f0))
@@ -34,10 +75,10 @@ function estimate_gplvm(input::ModelInput, num_components::Int=2)
 
     x_mu_gp_t = tf.transpose(x_mu_gp, perm=[1, 0])
     x_mu = tf.add(x_mu_gp_t, x_mu_bias)
-    # @show x_mu
-    # x = edmodels.Normal(loc=x_mu_gp_t, scale=x_sigma)
-    # x = edmodels.Normal(loc=x_mu_gp, scale=tf.expand_dims(x_sigma, -1))
-    # x_t = tf.transpose(x, perm=tf.constant([1,0]))
+    if correct_batch_effects
+        x_mu = tf.add(x_mu_batch, x_mu)
+    end
+
     x = edmodels.Normal(loc=x_mu, scale=x_sigma)
 
     likapprox = RNASeqApproxLikelihood(input, x)
@@ -52,7 +93,6 @@ function estimate_gplvm(input::ModelInput, num_components::Int=2)
     qmu_bias = edmodels.NormalWithSoftplusScale(loc=qmu_bias_loc,
                                                 scale=qmu_bias_softplus_scale)
 
-    # qz_loc = tf.Variable(tf.multiply(0.001, tf.random_normal([num_samples, num_components])))
     qz_loc = tf.Variable(tf.random_normal([num_samples, num_components]))
     qz_loc = tf_print_span(qz_loc, "qz_loc span")
     qz_softplus_scale = tf.Variable(tf.fill([num_samples, num_components], 0.0))
@@ -69,9 +109,18 @@ function estimate_gplvm(input::ModelInput, num_components::Int=2)
         bijector=tfdist.bijectors[:Exp](),
         name="LogNormalTransformedDistribution")
 
-    inference = ed.KLqp(Dict(z => qz, x_mu_gp => qx_mu_gp, x_mu_bias => qmu_bias,
-                             x_sigma_sq => qx_sigma_sq),
-                        data=Dict(likapprox => Float32[]))
+    vars = Dict(z => qz, x_mu_gp => qx_mu_gp, x_mu_bias => qmu_bias,
+                x_sigma_sq => qx_sigma_sq)
+
+    if correct_batch_effects
+        qw_batch_loc = tf.Variable(fill(0.0f0, (num_factors, n)))
+        qw_batch_softplus_scale = tf.Variable(tf.fill([num_factors, n], -1.0))
+        qw_batch = edmodels.NormalWithSoftplusScale(loc=qw_batch_loc, scale=qw_batch_softplus_scale)
+
+        vars[w_batch] = qw_batch
+    end
+
+    inference = ed.KLqp(vars, data=Dict(likapprox => Float32[]))
 
     optimizer = tf.train[:AdamOptimizer](0.05)
     run_inference(input, inference, 1500, optimizer)
@@ -91,3 +140,4 @@ end
 
 
 EXTRUDER_MODELS["gplvm"] = estimate_gplvm
+EXTRUDER_MODELS["batch-gplvm"] = input -> estimate_gplvm(input, correct_batch_effects=true)

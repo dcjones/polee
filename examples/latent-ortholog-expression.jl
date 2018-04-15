b2dcb6f47384a12823d9cde19e49c9578a63da95
Daniel C. Jones <dcjones@cs.washington.edu>
2018-04-14 18:09:52 -0700

    A little more work on corralling latent ortholog expression model.

--- examples/latent-ortholog-expression.jl
+++ examples/latent-ortholog-expression.jl
@@ -21,6 +21,7 @@ function main()
     species_transcripts_metadatas = Polee.TranscriptsMetadata[]
 
     # Load samples and transcripts for every species
+    num_samples = 0
     i = 2
     while i <= length(ARGS)
         if i + 1 > length(ARGS)
@@ -38,6 +39,8 @@ function main()
         push!(species_loaded_samples, loaded_samples)
         push!(species_transcripts, ts)
         push!(species_transcripts_metadatas, ts_metadata)
+
+        num_samples += length(loaded_samples.sample_names)
 
         i += 2
     end
@@ -75,15 +78,97 @@ function main()
         end
     end
     num_orth_groups = length(ortholog_groups)
-    print("Read ", num_orth_groups, " ortholog groups")
+    println("Read ", num_orth_groups, " ortholog groups")
 
-    # TODO: construct a sparse feature matrix for each species, similar to the
-    # one used for genes. Make sure to add aux groups for each ungrouped transcript
+    # for (k, (ts, ts_metadata)) in enumerate(zip(species_transcripts, species_transcripts_metadatas))
+        # species_ortholog_matrix(
+        #     num_orth_groups, ortholog_group_by_id, ts, ts_metadata)
+    # end
+
+    # Let' just start with point estimates for now.
+    x_og_mu0 = log(1/num_orth_groups)
+    x_og_sigma0 = 5.0
+    x_og = edmodels.Normal(
+        loc=tf.fill([num_samples, num_orth_groups], x_og_mu0),
+        scale=x_og_sigma0)
+
+    # slice x_og up by species
+    idxoff = 0
+    x_ogs = Any[]
+    for k in 1:num_taxons
+        ts = species_transcripts[k]
+        ts_metadata = species_transcripts_metadatas[k]
+
+        species_num_samples = length(species_loaded_samples[k].sample_names)
+        x_og_k = tf.slice(x_og, [idxoff, 0], [species_num_samples, num_orth_groups])
+        idxoff += species_num_samples
+
+        orthgroup_to_grouped_transcript, grouped_transcript_to_transcript =
+            species_ortholog_matrix(
+                num_orth_groups, ortholog_group_by_id, ts, ts_metadata)
+
+        x_gt = tf.transpose(tf.sparse_tensor_dense_matmul(orthgroup_to_grouped_transcript, tf.transpose(x_og_k)))
+        @show x_gt
+
+        # TODO: intra-group transcript mixtures
+
+        # TODO: non-grouped expression
+
+    end
+
+
 
     # TODO: set up intra-feature mixture variables.
 
     # TODO: feed that shit into approx likelihoods (again, seperate for each
     # species)
+end
+
+
+function species_ortholog_matrix(num_orth_groups, ortholog_group_by_id, ts, ts_metadata)
+    n = length(ts)
+
+    I = Int[]
+    J = Int[]
+    tid_to_grptid = Dict{Int, Int}()
+    grptid_to_tid = Dict{Int, Int}()
+    for t in ts
+        if haskey(ortholog_group_by_id, t.metadata.name)
+            push!(J, ortholog_group_by_id[t.metadata.name])
+            @assert !haskey(tid_to_grptid, t.metadata.id)
+            tid_to_grptid[t.metadata.id] = length(tid_to_grptid) + 1
+            grptid_to_tid[tid_to_grptid[t.metadata.id]] = t.metadata.id
+            push!(I, tid_to_grptid[t.metadata.id])
+        end
+    end
+
+    # sparse matrix mapping ortholog group index to grouped transcript index
+    orthgroup_to_grouped_transcript_indices =
+        Array{Int32}((length(I), 2))
+    for (k, (i, j)) in enumerate(zip(I, J))
+        orthgroup_to_grouped_transcript_indices[k, 1] = i - 1
+        orthgroup_to_grouped_transcript_indices[k, 2] = j - 1
+    end
+
+    orthgroup_to_grouped_transcript = tf.SparseTensor(
+        indices=orthgroup_to_grouped_transcript_indices,
+        values=tf.ones(length(I)),
+        dense_shape=[length(I), num_orth_groups])
+
+    # sparse matrix mapping grouped transcript index to transcript index
+    grouped_transcript_to_transcript_indices =
+        Array{Int32}((length(I), 2))
+    for (k, (i, j)) in enumerate(zip(I, J))
+        grouped_transcript_to_transcript_indices[k, 1] = i - 1
+        grouped_transcript_to_transcript_indices[k, 2] = grptid_to_tid[i] - 1
+    end
+
+    grouped_transcript_to_transcript = tf.SparseTensor(
+        indices=grouped_transcript_to_transcript_indices,
+        values=tf.ones(length(I)),
+        dense_shape=[n, length(I)])
+
+    return orthgroup_to_grouped_transcript, grouped_transcript_to_transcript
 end
 
 main()

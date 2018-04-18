d4969c2c4461e535f7aff49d849bb0787f3859a8
Daniel C. Jones <dcjones@cs.washington.edu>
2018-04-17 23:32:24 -0700

    Slight optimization to sparse matrix/vector multiply.

--- src/sparse.jl
+++ src/sparse.jl
@@ -3,34 +3,38 @@
 """
 Multithreaded version of At_mul_B!
 """
-function pAt_mul_B!{T,I}(y::Vector, A::SparseMatrixCSC{T,I}, x::Vector)
-    fill!(y, zero(T))
+function pAt_mul_B!{S,T,I}(y::Vector{S}, A::SparseMatrixCSC{T,I}, x::Vector)
     colptr = A.colptr
     rowval = A.rowval
     nzval = A.nzval
 
     Threads.@threads for j in 1:length(y)
-        @inbounds for k_ in colptr[j]:colptr[j+1]-1
-            k = Int(k_)
-            i = Int(rowval[k])
-            y[j] += x[i] * nzval[k]
+        @inbounds begin
+            y[j] = zero(T)
+            for k_ in colptr[j]:colptr[j+1]-1
+                k = Int(k_)
+                i = Int(rowval[k])
+                y[j] += x[i] * nzval[k]
+            end
         end
     end
 end
 
 
 # multiply by 1./x instead of x
-function pAt_mulinv_B!{T,I}(y::Vector, A::SparseMatrixCSC{T,I}, x::Vector)
-    fill!(y, zero(T))
+function pAt_mulinv_B!{S,T,I}(y::Vector{S}, A::SparseMatrixCSC{T,I}, x::Vector)
     colptr = A.colptr
     rowval = A.rowval
     nzval = A.nzval
 
     Threads.@threads for j in 1:length(y)
-        @inbounds for k_ in colptr[j]:colptr[j+1]-1
-            k = Int(k_)
-            i = Int(rowval[k])
-            y[j] += nzval[k] / x[i]
+        @inbounds begin
+            y[j] = zero(T)
+            for k_ in colptr[j]:colptr[j+1]-1
+                k = Int(k_)
+                i = Int(rowval[k])
+                y[j] += nzval[k] / x[i]
+            end
         end
     end
 end

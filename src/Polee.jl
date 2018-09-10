211f63aee0d18af04cf0f635beb08fbcda4dcb43
Daniel C. Jones <dcjones@cs.washington.edu>
2018-09-10 14:59:52 -0700

    Fix building tf extension.

--- src/Polee.jl
+++ src/Polee.jl
@@ -18,6 +18,21 @@ using Pkg
 tfutil.reveal_undocumented("edward.util.graphs")
 @pyimport edward.util.graphs as edgraphs
 
+# check if the tensorflow extension exists and bulid it if it doesn't.
+function build_tensorflow_ext_if_needed()
+    ext_path = joinpath(dirname(pathof(Polee)), "tensorflow_ext")
+    ext_lib_filename = joinpath(ext_path, "hsb_ops.so")
+    ext_src_filename = joinpath(ext_path, "hsb_ops.cpp")
+    if !isfile(ext_lib_filename)
+        @warn "Attempting to build tensorflow extension automatically."
+        cflags = chomp(String(read(`python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))'`)))
+        lflags = chomp(String(read(`python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))'`)))
+        cmd = "g++ -std=c++11 -shared -g -O2 $(ext_src_filename) -o $(ext_lib_filename) -fPIC $(cflags) $(lflags)"
+        println(cmd)
+        run(Cmd(Vector{String}(split(cmd))))
+    end
+end
+
 build_tensorflow_ext_if_needed()
 pushfirst!(PyVector(pyimport("sys")["path"]), dirname(pathof(Polee)))
 @pyimport polee as polee_py
@@ -55,21 +70,6 @@ pushfirst!(PyVector(pyimport("sys")["path"]), dirname(pathof(Polee)))
 #     build_tensorflow_ext_if_needed()
 # end
 
-
-# check if the tensorflow extension exists and bulid it if it doesn't.
-function build_tensorflow_ext_if_needed()
-    ext_path = joinpath(dirname(pathof(Polee)), "tensorflow_ext")
-    ext_lib_filename = joinpath(ext_path, "hsb_ops.so")
-    ext_src_filename = joinpath(ext_path, "hsb_ops.cpp")
-    if !isfile(ext_lib_filename)
-        @warn "Attempting to build tensorflow extension automatically."
-        cflags = chomp(String(read(`python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_compile_flags()))'`)))
-        lflags = chomp(String(read(`python -c 'import tensorflow as tf; print(" ".join(tf.sysconfig.get_link_flags()))'`)))
-        cmd = "g++ -std=c++11 -shared -g -O2 $(ext_src_filename) -o $(ext_lib_filename) -fPIC $(cflags) $(lflags)"
-        println(cmd)
-        run(Cmd(Vector{String}(split(cmd))))
-    end
-end
 
 
 using ArgParse

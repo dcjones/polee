abb57e9129cdeef1c8848e6a8602484aba5a44cd
Daniel C. Jones <dcjones@cs.washington.edu>
2020-02-18 16:29:25 -0800

    Start adding some tests.

---
+++ test/runtests.jl
@@ -0,0 +1,31 @@
+
+using Test
+using Polee
+
+
+@testset "Likelihood approximation w/ genome alignments" begin
+    args = String[
+        "prep-sample",
+        "dataset/genome.fa",
+        "dataset/mBr_M_6w_1.genome.bam",
+        "dataset/annotations.gff3" ]
+    Polee.main(args)
+end
+
+
+@testset "Likelihood approximation w/ transcriptome alignments" begin
+    args = String[
+        "prep-sample",
+        "dataset/transcriptome.fa",
+        "dataset/mBr_M_6w_1.transcriptome.bam" ]
+    Polee.main(args)
+end
+
+# TODO: test polee sample
+
+# TODO: test polee debug-sample
+
+# TODO: some tests for Transcripts
+
+# TODO: some tests for Reads
+
